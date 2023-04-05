8f77243e78b4d4f54bb853ee025a113b18ab3d2e
Damego <danyabatueff@gmail.com>
2023-04-05 21:18:35 +0500

    chore: remove wildcards from middleware

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Set-Cookie"],
 )
 
 app.include_router(anime.router)
